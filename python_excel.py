64d3fa1f03a41270de85cb697caac72d45ca13be
chiale_li <lijiale@asterfusion.com>
2020-01-19 17:01:04 +0800

    copy to github

--- python_excel.py
+++ python_excel.py
@@ -2,20 +2,30 @@
 import xlrd
 import xlwt
 
-data = xlrd.open_workbook('成绩单.xls')
+read_sheets():
+    data = xlrd.open_workbook('成绩单.xls')
+    print('所有表格数量：{}'.format(data.nsheets))
+    for sheet_num in range(data.nsheets):
+        sheet_name = data.sheet_names()[sheet_num]
+        print('\n')
+        print(sheet_name)
+        print("#########")
 
-sheet_num = data.nsheets
-print('所有表格数量：{}'.format(sheet_num))
+        sheet = data.sheet_by_index(sheet_num)
+        print('表格名称:{}\n表格列数: {}\n表格行数: {}'.format(sheet.name, sheet.ncols, sheet.nrows))
+        for col_num in range(sheel.ncols):
+            col_name = sheel.cell(0, col_num)self.
+        for row_num in range(1, sheet.nrows):
+            for col_num in range(sheet.ncols):
+                value = sheet.cell(row_num, col_num).value
+                #if (len(sheet.cell(j, k)) != 0):
+                print(sheet.cell(row_num, col_num).value)
+                #else: 
+                #    print("null")
+write_sheets():
 
-sheet_name = data.sheet_names()[0]  # 获取所有sheet名称
-print(sheet_name)
 
-sheet_name = data.sheet_names()[1]  # 获取所有sheet名称
-print(sheet_name)
+if __name == "__main__"
+    read_sheets()
+    write_sheets()
 
-sheet = data.sheet_by_index(0)
-print('表格名称:{}\n表格列数: {}\n表格行数: {}'.format(sheet.name, sheet.ncols, sheet.nrows))
-
-for i in range(sheet.nrows):
-    for j in range(sheet.ncols):
-        print(sheet.cell(i,j).value)
